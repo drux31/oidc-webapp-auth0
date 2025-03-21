04c66f146f8be6ecaccd60e9e4e62dcbeac3a36a
Landry N. <landry.ntsama@devops.ncia.nato.int>
2025-03-21 15:14:37 +0000

    setting up Auth0 authorisation server)

--- src/server.js
+++ src/server.js
@@ -59,6 +59,24 @@ app.get('/remove-to-do/:id', async (req, res) => {
   res.status(501).send();
 });
 
+const { OIDC_PROVIDER } = process.env;
+const discEnd = 'https://${OIDC_PROVIDER}/.well-known/openid-configuration';
+
+request(discEnd)
+  .then(res => {
+    var oidcProviderInfo = JSON.parse(res);
+
+    app.listen(3000, () => {
+      console.log('Server running on http://localhost:3000');
+    });
+  })
+  .catch(error => {
+    console.error(error);
+    console.error('Unable to get OIDC endpoints for ${OIDC_PROVIDER}');
+    process.exit(1);
+  });
+/*
 app.listen(3000, () => {
   console.log(`Server running on http://localhost:3000`);
 });
+*/
