edfa646646c6bf5279b1c738570ad26b58bffe35
Landry N. <landry.ntsama@devops.ncia.nato.int>
2025-03-24 10:43:57 +0100

    Initiating the authentication process

--- src/server.js
+++ src/server.js
@@ -44,7 +44,41 @@ app.get('/profile', (req, res) => {
 });
 
 app.get('/login', (req, res) => {
-  res.status(501).send();
+  //res.status(501).send();
+  //define constants for the authorisation request
+  const authorisationEndpoint = oidcProviderInfo['authorization_endpoint']; // authorisation URL where the user are redirected
+  const responseType = 'id_token'; // the response type expected from the prodiver
+  const scope = 'openid'; // the information needed about the user authenticating
+  const clientID = process.env.CLIENT_ID; //the identifier that the provider attributes to the application
+  const redirectUri = process.env.REDIRECT_URI; // where the provider redirects the user after the authentication
+  const responseMode = 'form_post'; // how the application will get the ID token for the end user
+  const nonce = crypto.randomBytes(16).toString('hex'); // random string that helps the application to prevent from replay attacks
+
+  //define a signed cookie containing the nonce value
+  const options = {
+    maxAge: 1000 * 60 * 15,
+    httpOnly: true, // the cookie onky accessible by the web server
+    signed: true // indicate if the cookie should be signed
+  };
+
+  // add the cookie to the response and issue a 302 redirecting user
+  res
+    .cookie(nonce, options)
+    .redirect(
+      authorisationEndpoint +
+        '?response_mode=' +
+        responseMode +
+        '&response_type=' +
+        responseType +
+        '&scope=' +
+        scope +
+        '&client_id=' +
+        clientID +
+        '&redirect_uri=' +
+        redirectUri +
+        '&nonce=' +
+        nonce
+    );
 });
 
 app.post('/callback', async (req, res) => {
@@ -59,12 +93,13 @@ app.get('/remove-to-do/:id', async (req, res) => {
   res.status(501).send();
 });
 
-const { OIDC_PROVIDER } = process.env;
-const discEnd = 'https://${OIDC_PROVIDER}/.well-known/openid-configuration';
+const OIDC_PROVIDER = process.env.OIDC_PROVIDER;
+const discEnd =
+  'https://' + OIDC_PROVIDER + '/.well-known/openid-configuration';
 
 request(discEnd)
   .then(res => {
-    var oidcProviderInfo = JSON.parse(res);
+    oidcProviderInfo = JSON.parse(res);
 
     app.listen(3000, () => {
       console.log('Server running on http://localhost:3000');
@@ -72,7 +107,7 @@ request(discEnd)
   })
   .catch(error => {
     console.error(error);
-    console.error('Unable to get OIDC endpoints for ${OIDC_PROVIDER}');
+    console.error('Unable to get OIDC endpoints for ' + OIDC_PROVIDER);
     process.exit(1);
   });
 /*
